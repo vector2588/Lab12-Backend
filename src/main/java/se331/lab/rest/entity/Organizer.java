0b20d34dfb05352b6fb21b530e299c43a918e463
Chartchai Doungsa-ard <chartchai.d@cmu.ac.th>
2021-09-30 00:52:15 +0700

    initial commit

---
+++ src/main/java/se331/lab/rest/entity/Organizer.java
@@ -0,0 +1,23 @@
+package se331.lab.rest.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Organizer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Exclude
+    Long id;
+    String name;
+    @OneToMany(mappedBy = "organizer")
+    @Builder.Default
+    List<Event> ownEvents = new ArrayList<>();
+}
---
+++ src/main/java/se331/lab/rest/util/LabMapper.java
@@ -0,0 +1,22 @@
+package se331.lab.rest.util;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+import se331.lab.rest.entity.Event;
+import se331.lab.rest.entity.EventDTO;
+import se331.lab.rest.entity.Organizer;
+import se331.lab.rest.entity.OrganizerDTO;
+
+import java.util.List;
+
+@Mapper
+public interface LabMapper {
+    LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
+    EventDTO getEventDto(Event event);
+    List<EventDTO> getEventDto(List<Event> events);
+
+    OrganizerDTO getOrganizerDTO(Organizer organizer);
+    List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
+
+
+}
