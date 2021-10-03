c2e86395b1e8b44a4750a654f1593c30514bacee
Vector2588 <natkamol082@gmail.com>
2021-10-03 23:29:14 +0700

    6.8 Block add event user

--- src/main/java/se331/lab/rest/util/LabMapper.java
+++ src/main/java/se331/lab/rest/util/LabMapper.java
@@ -1,15 +1,14 @@
 package se331.lab.rest.util;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
-import se331.lab.rest.entity.Event;
-import se331.lab.rest.entity.EventDTO;
-import se331.lab.rest.entity.Organizer;
-import se331.lab.rest.entity.OrganizerDTO;
+import se331.lab.rest.entity.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
-@Mapper
+@Mapper(imports = Collectors.class)
 public interface LabMapper {
     LabMapper INSTANCE = Mappers.getMapper(LabMapper.class);
     EventDTO getEventDto(Event event);
@@ -17,6 +16,10 @@ public interface LabMapper {
 
     OrganizerDTO getOrganizerDTO(Organizer organizer);
     List<OrganizerDTO> getOrganizerDTO(List<Organizer> organizers);
+
+    @Mapping(target = "authorities", expression = "java(organizer.getUser().getAuthorities().stream().map(auth -> auth.getName().name()).collect(Collectors.toList()))")
+    OrganizerAuthDTO getOrganizerAuthDTO(Organizer organizer);
+
 
 
 }
