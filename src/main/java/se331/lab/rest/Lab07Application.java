8d9dc7ec9790edb2b3420ec9dfdecbccd16391ff
Vector2588 <natkamol082@gmail.com>
2021-10-10 22:03:25 +0700

    Deploy again

--- src/main/java/se331/lab/rest/Lab07Application.java
+++ src/main/java/se331/lab/rest/Lab07Application.java
@@ -19,7 +19,7 @@ public class Lab07Application {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("http://10.2.0.4:8001")
                         .exposedHeaders("x-total-count");
             }
         };
